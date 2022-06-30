1a42c999a8a2ed1abb26b0ffeb7a18d790b1e4a3
chen dong <chendong.jy@gmail.com>
2022-06-30 18:21:44 +1200

    add testing

--- test/test_actions.py
+++ test/test_actions.py
@@ -1,10 +1,8 @@
 from operator import le
-from coordinates import Coordinate
 import pytest
 from model.wall import Wall, WallInfo
-from action.actions import create_wall
-from model.timber import TopPlate, BottomPlate, LeftOuterStud, RightOuterStud, Plate
-from model.direction import Orientation
+from action.actions import create_wall, add_top_plate
+from model.timber import TopPlate, Plate
 from utility.location import XYCoordinate
 
 
@@ -39,3 +37,18 @@ def test_move_right_timber():
     timber = Plate(1330)
     timber.move_right(30)
     assert timber.a_cord == XYCoordinate(30, 0)
+
+def test_add_top_plate():
+    wall: Wall = create_wall(Wall(WallInfo(440, 330)))
+    wall = add_top_plate(wall)
+    top_plate: TopPlate = wall.top_plate
+    assert top_plate.a_cord == XYCoordinate(0, 285)
+
+def test_wall_cord():
+    wall: Wall = create_wall(Wall(WallInfo(440, 330)))
+    assert wall.c_cord == XYCoordinate(440, 330)
+
+def test_wall_cord_minus_timber_cord():
+    wall: Wall = create_wall(Wall(WallInfo(440, 330)))
+    plate: Plate = Plate(200)
+    assert (wall.b_cord - plate.b_cord).norm() == 240
