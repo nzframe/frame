c676469e47f3fffbfd7afe9485d9d265cc1b0228
chen dong <chendong.jy@gmail.com>
2022-06-30 16:44:01 +1200

    timber coordinate testing

--- test/test_timber.py
+++ test/test_timber.py
@@ -1,5 +1,6 @@
 import pytest
 from model.timber import CuttedTimber, LeftOuterStud, Orientation, Stud, Plate, TopPlate, LengthLessThanZeroError
+from utility.location import XYCoordinate
 
 
 def test_cutted_timber_default_value():
@@ -29,3 +30,15 @@ def test_timber_purpuse_leftstud():
 def test_timber_length_less_than_0():
     with pytest.raises(LengthLessThanZeroError):
         TopPlate(-1)
+
+def test_timber_coordinate():
+    cutted_timber = CuttedTimber(2330)
+    assert cutted_timber.c_cord == None
+
+def test_plate_coordinate():
+    plate = Plate(1380)
+    assert plate.c_cord == XYCoordinate(1380, 45)
+
+def test_stud_coordinate():
+    stud = Stud(1380)
+    assert stud.c_cord == XYCoordinate(45, 1380)
