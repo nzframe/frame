07b6d8b4eb14e535e39567e13e671a08a1967547
chen dong <chendong.jy@gmail.com>
2022-06-30 21:03:07 +1200

    improve the color

--- test/draw.py
+++ test/draw.py
@@ -4,7 +4,7 @@ from model.wall import Wall, WallInfo
 from model.timber import CuttedTimber
 
 
-wall: Wall = create_wall(Wall(WallInfo(440, 330)))
+wall: Wall = create_wall(Wall(WallInfo(660, 330)))
 
 d = draw.Drawing(1024, 768, origin=(0,0), displayInline=False)
 
@@ -14,14 +14,14 @@ for timber in wall.timbers():
     width =  (timber.b_cord - timber.a_cord).norm()
     height = (timber.c_cord - timber.b_cord).norm()
     # Draw a rectangle
-    r = draw.Rectangle(x,y,width,height, fill='#1248ff')
+    r = draw.Rectangle(x,y,width,height, fill='#3b3a47', stroke_width=2, stroke='black')
     r.appendTitle("Our first rectangle")  # Add a tooltip
 
     d.append(r)
 
 d.setPixelScale(2)  # Set number of pixels per geometry unit
 #d.setRenderSize(400,200)  # Alternative to setPixelScale
-#d.saveSvg('example.svg')
+d.saveSvg('example.svg')
 d.savePng('example.png')
 
 d.rasterize()  # Display as PNG
