f0247604ad088f92189d57e9612a4088d4e1c62c
chen dong <chendong.jy@gmail.com>
2022-06-30 15:28:00 +1200

    add basic drawing

---
+++ utility/draw.py
@@ -0,0 +1,30 @@
+import drawSvg as draw
+from action.actions import create_wall
+from model.wall import Wall, WallInfo
+from model.timber import CuttedTimber
+
+
+wall: Wall = create_wall(Wall(WallInfo(2400, 2550)))
+
+d = draw.Drawing(10000, 5000, origin='center', displayInline=False)
+
+for timber in wall.timbers():
+    timber: CuttedTimber = timber
+    x, y = timber.a_cord.to_list()
+    width =  (timber.b_cord - timber.a_cord).norm()
+    height = (timber.c_cord - timber.b_cord).norm()
+
+    # Draw a rectangle
+    r = draw.Rectangle(x,y,width,height, fill='#1248ff')
+    r.appendTitle("Our first rectangle")  # Add a tooltip
+
+    d.append(r)
+
+d.setPixelScale(2)  # Set number of pixels per geometry unit
+#d.setRenderSize(400,200)  # Alternative to setPixelScale
+d.saveSvg('example.svg')
+d.savePng('example.png')
+
+d.rasterize()  # Display as PNG
+d  # Display as SVG
+
