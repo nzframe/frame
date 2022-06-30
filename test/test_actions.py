d34f529b949606706e7a1feb044d27f791a3022c
chen dong <chendong.jy@gmail.com>
2022-06-30 14:47:17 +1200

    remove blanks

--- test/test_actions.py
+++ test/test_actions.py
@@ -8,13 +8,9 @@ from model.timber import TopPlate, BottomPlate, LeftOuterStud, RightOuterStud
 def test_create_wall():
     wall = create_wall(Wall(WallInfo(2400, 2550)))
     assert wall.get_total_timbers() == 4
-    
     assert wall.top_plate.length == 2400
-
     assert wall.bottom_plate.length == 2400
-
     assert wall.left_outer_stud.length == 2460
-
     assert wall.right_outer_stud.length == 2460
 
 def test_type_of_cutted_timber():
