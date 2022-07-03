33e8d655dfd609f7e7a430b50e86e224f11cfde4
chen dong <chendong.jy@gmail.com>
2022-07-04 11:02:27 +1200

    add interface of action

---
+++ action/action.py
@@ -0,0 +1,29 @@
+def move_components():
+    pass
+
+def add_header():
+    pass
+
+def add_still():
+    pass
+
+def add_cripple():
+    pass
+
+def add_kind_stud():
+    pass
+
+def add_trimmer_stud():
+    pass
+
+def add_top_plate():
+    pass
+
+def add_bottom_plate():
+    pass
+
+def add_left_kind_stud():
+    pass
+
+def add_right_kind_stud():
+    pass
