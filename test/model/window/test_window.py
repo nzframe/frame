333d7fb2e4db432216dd379704a38826daefee5d
chen dong <chendong.jy@gmail.com>
2022-07-08 14:36:32 +1200

    add dry door move right testing

--- test/model/window/test_window.py
+++ test/model/window/test_window.py
@@ -14,3 +14,13 @@ def test_pairwise():
     from more_itertools import pairwise
     a = [1,2,3]
     assert [(x, y) for x, y in pairwise(a)] == [(1, 2), (2, 3)]
+
+def test_draw_dry_door_move_right():
+    window = Window(2170, 1095, 630, 2630)
+    window.move_right(300)
+    from utility.draw import DrawIT
+    from pathlib import Path
+    file_path = Path(__file__).parent / "window_door_move_right.png" 
+    td = DrawIT(file_path.as_posix())
+    
+    draw_window(td, window)
