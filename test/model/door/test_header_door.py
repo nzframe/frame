e4b9c0b0250eb8077fc936cbe5ab3abb3f5e8eed
chen dong <chendong.jy@gmail.com>
2022-07-08 14:28:13 +1200

    test move right

--- test/model/door/test_header_door.py
+++ test/model/door/test_header_door.py
@@ -10,3 +10,13 @@ def test_draw_header_door():
     td = DrawIT(file_path.as_posix())
     
     draw_header_door(td, door)
+
+def test_draw_header_door_move_right():
+    door = HeaderDoor(800, 2220, 2310)
+    door.move_right(300)
+    from utility.draw import DrawIT
+    from pathlib import Path
+    file_path = Path(__file__).parent / "header_door_move_right.png" 
+    td = DrawIT(file_path.as_posix())
+    
+    draw_header_door(td, door)
