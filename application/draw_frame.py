16c7c8a0dfaa42eb7827111dacdc09e1b953f8ba
chen dong <chendong.jy@gmail.com>
2022-07-09 23:55:03 +1200

    refactor draw_frame

--- application/draw_frame.py
+++ application/draw_frame.py
@@ -1,86 +1,115 @@
-from psutil import boot_time
+from dataclasses import dataclass
 from model.direction import Orientation
 from model.timber import Cutted2BY4
 from utility.draw import DrawIT
 from pathlib import Path
 import yaml
-import importlib
-
-from model.generic_wall import GenericWall
-from utility.space.cord import XYCoordinate
-from utility.space.rect import XYRectangle
-from model.timber import CuttedTimber
 import copy
 
 frame_photo_path = Path(__file__).parent / "frame.png" 
 config_path = Path(__file__).parent / "config.yml" 
 td = DrawIT(frame_photo_path.as_posix())
 
-config = {
-    "LintelDoor": "model.door",
-    "HeaderDoor": "model.door",
-    "DryDoor": "model.door",
-    "CommonWall": "model.common",
-    "Junction": "model.junction",
-    "Window": "model.window",
-    "LoadPoint": "model.load_point"
-}
 
-def load_yaml(file_path: str):
+def load_config(file_path: str):
     with open(file_path, 'r') as stream:
         rt = yaml.safe_load(stream)
     return rt
 
-def prepare_cutted_timber(ct: CuttedTimber, td: DrawIT):
-    td.prepare(ct)
+def get_class_dict():
+    import importlib
+    configs = {
+        "LintelDoor": "model.door",
+        "HeaderDoor": "model.door",
+        "DryDoor": "model.door",
+        "CommonWall": "model.common",
+        "Junction": "model.junction",
+        "Window": "model.window",
+        "LoadPoint": "model.load_point"
+    }
+    class_dict = {}
+    for class_name, module_name in configs.items():
+        class_dict[class_name] = getattr(importlib.import_module(module_name), class_name)
+    return class_dict
+
+@dataclass
+class AppInfo:
+    title: str
+    floor_height: float
+
+class App:
+    def __init__(self, file_path) -> None:
+        self.wall_global_info = self.__get_wall_global_info(file_path)
+        self.wall_detailed_info = self.__get_wall_detailed_info(file_path)
+
+        self.current_move = 0
+        self.instances = []
+
+    def get_start_point(self):
+        return self.instances[0].get_area.a_cord.x
+
+    def get_end_point(self):
+        return self.instances[-1].get_area.b_cord.x
+
+    def main(self):
+        self.init_each_instance()
+
+        # move first
+        for instance in self.instances:
+            instance.move_right(self.current_move)
+            self.current_move = self.current_move + instance.get_area.b_cord.x - instance.get_area.a_cord.x
+        
+        # after move, then get the location and get the end point
+        plate_size = self.get_end_point() - self.get_start_point()
+        plates = self.add_plates(plate_size, self.wall_global_info.floor_height)
+
+        #draw
+        self.draw(td, plates)
+
+
+    def draw(self, td: DrawIT, plates):
+        for instance in self.instances:
+            instance.draw(td)
+        # draw plates
+        for plate in plates:
+            td.prepare(plate)
+
+        td.draw_it()
+
+    def init_each_instance(self):
+        for config in self.wall_detailed_info:
+            type_name = config.pop("type")
+            config["floor_height"] = self.wall_global_info.floor_height
+            self.instances.append(self.get_wall_instance(type_name, config))
+
+    def get_wall_instance(self, type_name, args):
+        class_dict = get_class_dict()
+        assert len(class_dict) == 7
+        wall = class_dict[type_name](**args)
+        
+        return wall
+
+    def __get_wall_global_info(self, file_path):
+        rt = load_config(file_path)
+        title = rt["title"]
+        floor_height = rt["floor_height"]
+        return AppInfo(title, floor_height)
+
+    def __get_wall_detailed_info(self, file_path):
+        rt = load_config(file_path)
+        return rt["parts"]
+
+    def add_plates(self, plate_size, floor_height):
+        bottom_plate = Cutted2BY4(plate_size, Orientation.HORIZONTAL)
+
+        top_plate = copy.copy(bottom_plate)
+        top_plate.move_up(Cutted2BY4.HEIGHT + floor_height)
+        
+        return [top_plate, bottom_plate]
 
 def main():
-    rt = load_yaml(config_path.as_posix())
-    title = rt["title"]
-    floor_height = rt["floor_height"]
-
-    total_right_move = 0
-
-    
-    index = 0
-    start_point: XYCoordinate = None
-    end_point: XYCoordinate = None
-    for ele in rt["parts"]:
-        class_name = ele.pop("type")
-        module_name = config[class_name]
-
-        the_class = getattr(importlib.import_module(module_name), class_name)
-        instance: GenericWall = the_class(floor_height = floor_height, **ele)
-        try:
-            instance.move_right(total_right_move)
-        except AttributeError:
-            print(f"Ingore {class_name} move right function")
-
-        instance.draw(td)
-
-        rect: XYRectangle = instance.get_area
-        total_right_move += (rect.b_cord.x - rect.a_cord.x)
-
-        if index == 0:
-            start_point = rect.a_cord.x
-        elif index == (len(rt["parts"]) - 1):
-            end_point = rect.b_cord.x
-            # load_point = LoadPoint(2630, 1)
-            # junction = Junction(2630, 3)
-            # common_wall = CommonWall(600, 2630)
-            # lintel_door = LintelDoor(930, 2170, 2630)
-            # common_wall2 = CommonWall(600, 2630)
-        index = index + 1
-
-    plate_size = end_point - start_point
-    bottom_plate = Cutted2BY4(plate_size, Orientation.HORIZONTAL)
-    prepare_cutted_timber(bottom_plate, td)
-
-    top_plate = copy.copy(bottom_plate)
-    top_plate.move_up(Cutted2BY4.HEIGHT + floor_height)
-    prepare_cutted_timber(top_plate, td)
-
-    td.draw_it()
+    app = App(config_path)
+    app.main()
     
 if __name__ == "__main__":
     main()
--- application/test_app.py
+++ application/test_app.py
@@ -1,7 +1,46 @@
 import pytest
+from draw_frame import get_class_dict
+
 
 def test_func_params():
     def fun(a, b):
         return a < b
 
     assert fun(b=2, a=1) == True
+
+def test_get_class_dict():
+    assert len(get_class_dict()) == 7
+
+def test_function_pass():
+    config = {'wall_width': 785}
+    def fun(wall_width):
+        return wall_width
+    assert fun(**config) == 785 
+
+def get_class_dict():
+    import importlib
+    configs = {
+        "LintelDoor": "model.door",
+        "HeaderDoor": "model.door",
+        "DryDoor": "model.door",
+        "CommonWall": "model.common",
+        "Junction": "model.junction",
+        "Window": "model.window",
+        "LoadPoint": "model.load_point"
+    }
+    class_dict = {}
+    for class_name, module_name in configs.items():
+        class_dict[class_name] = getattr(importlib.import_module(module_name), class_name)
+    return class_dict
+
+def get_wall_instance(type_name, args):
+    class_dict = get_class_dict()
+    assert len(class_dict) == 7
+    wall = class_dict[type_name](**args)
+    
+    return wall
+
+def test_get_wall_instance():
+    config = {'wall_width': 785, 'floor_height': 200}
+    instance = get_wall_instance("CommonWall", config)
+    assert instance is not None
